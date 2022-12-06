4fdf10997a71a2fec04e9664063a36a5498571d1
danielbom <daniel.rodrigues.45@hotmail.com>
2022-12-06 04:38:15 -0300

    feat(form-state): improve the presentation

--- src/app/components/form-state/form-state.component.ts
+++ src/app/components/form-state/form-state.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core'
-import { AbstractControl } from '@angular/forms'
+import { AbstractControl, FormGroup } from '@angular/forms'
 
 type FieldError = {
   name: string
@@ -12,7 +12,7 @@ type FieldError = {
   styleUrls: ['./form-state.component.scss'],
 })
 export class FormStateComponent implements OnInit {
-  @Input() control: AbstractControl | null = null
+  @Input() control?: AbstractControl
 
   show = true
 
@@ -24,11 +24,45 @@ export class FormStateComponent implements OnInit {
     this.show = !this.show
   }
 
-  // computed
-  get errors(): FieldError[] {
-    return Object.entries(this.control?.errors || {}).map(([name, value]) => ({
-      name,
+  logForm() {
+    console.log(this.control)
+  }
+
+  _extractErrors(
+    prefix: string,
+    control: AbstractControl | undefined,
+  ): FieldError[] {
+    return Object.entries(control?.errors || {}).map(([name, value]) => ({
+      name: prefix + name,
       value,
     }))
+  }
+
+  _extractErrorsRec(
+    prefix: string,
+    control: AbstractControl | undefined,
+  ): FieldError[] {
+    if (!control) return []
+
+    const errors: FieldError[] = this._extractErrors(prefix, control)
+
+    const controls: Record<string, AbstractControl> = (control as FormGroup)
+      .controls
+    if (!controls) return errors
+
+    const innerErrors = Object.entries(controls).flatMap(([name, value]) =>
+      this._extractErrorsRec(prefix + name + '.', value),
+    )
+
+    return errors.concat(innerErrors)
+  }
+
+  // computed
+  get errors(): FieldError[] {
+    return this._extractErrorsRec('_.', this.control)
+  }
+
+  get formGroup(): FormGroup | undefined {
+    return this.control as FormGroup
   }
 }
--- src/app/components/form-state/item-status/item-status.component.ts
+++ src/app/components/form-state/item-status/item-status.component.ts
@@ -3,10 +3,9 @@ import { Component, ChangeDetectionStrategy, Input } from '@angular/core'
 @Component({
   selector: 'app-item-status',
   templateUrl: './item-status.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ItemStatusComponent {
-  @Input() label: string = ""
+  @Input() label: string = ''
   @Input() value: boolean = true
 }
-
